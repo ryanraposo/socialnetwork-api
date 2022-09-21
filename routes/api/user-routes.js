81c6da87bf8549b8bafef9a5a016530962f4530d
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-21 19:20:03 -0400

    add user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
+
+
+router.route('/').get(getAllUsers);
+router.route('/:id').get(getUserById);
+router.route('/').post(createUser);
+router.route('/:id').put(updateUser);
+router.route('/:id').delete(deleteUser);
+
+
+module.exports = router;
