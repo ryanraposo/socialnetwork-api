53e66bb9c7801ec7651a1582d315482a17939181
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-21 18:43:53 -0400

    create user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      },
+      thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+      ],
+      friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true
+      },
+      id: false
+    }
+);
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+
+const User = model('User', UserSchema);
+
+
+module.exports = User;
